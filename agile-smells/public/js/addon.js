073f6b683049a72082d9db6405e3d0107a6598c7
Matthew Brand <mbrand@gmail.com>
2018-04-02 16:14:30 -0600

    More robust added logic

--- agile-smells/public/js/addon.js
+++ agile-smells/public/js/addon.js
@@ -39,6 +39,7 @@ function setData() {
 }
 
 var addedIssues = {};
+var currentProcessingSprintID = 0;
 
 function getAllSprints(boardID) {
   //TODO: this breaks if there are more than 50 sprints. This should be tied to when we let them configure the number of sprints to look back and don't let them choose greater than 50
@@ -65,73 +66,93 @@ function getAllSprints(boardID) {
     });
 }
 
-function setStoriesAdded(sprintID) {
+function setStoriesAdded(currentProcessingSprintID) {
   AP.request({
       //TODO: breaks after 50 issues, need pagination
       //future sprints are not needed
-      url: '/rest/agile/1.0/sprint/' + sprintID + '/issue?expand=changelog&fields=changelog,sprint,created,closedSprints',
-      success: checkForAddedIssues,
+      url: '/rest/agile/1.0/sprint/' + currentProcessingSprintID + '/issue?expand=changelog&fields=changelog,sprint,created,closedSprints,creator',
+      success: function(response) {
+        checkForAddedIssues(response, currentProcessingSprintID);
+      },
       error: function() {
         console.log(arguments);
       }
     });
 }
 
-function checkForAddedIssues(response) {
+function checkForAddedIssues(response, currentProcessingSprintID) {
   // convert the string response to JSON
   response = JSON.parse(response);
 
   for (var i = 0; i < response.issues.length; i++) {
     var issue = response.issues[i];
-    checkActiveSprintAsPartOfCreation(issue);
-    checkClosedSprintAsPartOfCreation(issue);
-    checkAddedToActiveSprintAfterCreated(issue);
-    checkAddedToClosedSprintAfterCreated(issue);
+    checkActiveSprintAsPartOfCreation(issue, currentProcessingSprintID);
+    checkClosedSprintAsPartOfCreation(issue, currentProcessingSprintID);
+    checkAddedToActiveSprintAfterCreated(issue, currentProcessingSprintID);
+    checkAddedToClosedSprintAfterCreated(issue, currentProcessingSprintID);
   }
   console.log(addedIssues);
 }
 
-function checkActiveSprintAsPartOfCreation(issue) {
-  if(issue.fields.sprint != null) {
+function checkActiveSprintAsPartOfCreation(issue, currentProcessingSprintID) {
+  if(issue.fields.sprint != null && issue.fields.sprint.id == currentProcessingSprintID) {
     checkSprintAsPartOfCreation(issue, issue.fields.sprint)
   }
 }
 
 function checkSprintAsPartOfCreation(issue, sprint) {
-  var createdTimestamp = issue.fields.created;
-  var sprintCreatedTimestamp = sprint.startDate;
+  //thanks JIRA for storing some dates in UTC and some in local time. sprint start date is stored UTC
+  var timeZone = issue.fields.creator.timeZone;
+  var createdTimestamp = moment(issue.fields.created).tz(timeZone);
+  var sprintCreatedTimestamp = moment(sprint.startDate);
   if (createdTimestamp > sprintCreatedTimestamp) {
-    addedIssues[sprint.id + '-' + issue.id] = issue.key;
-  }
-}
-
-function checkClosedSprintAsPartOfCreation(issue) {
-  if(issue.fields.closedSprints != null) {
-    for(var i = 0; i <issue.fields.closedSprints.length; i++) {
-      checkSprintAsPartOfCreation(issue, issue.fields.closedSprints[i]);
+    if(addedIssues[sprint.id] == null) {
+      addedIssues[sprint.id] = {};
+      addedIssues[sprint.id][issue.id] = issue.key;//{id:issue.id, key:issue.key};
+    }
+    else {
+      addedIssues[sprint.id][issue.id] = issue.key;
     }
   }
 }
 
-function checkAddedToActiveSprintAfterCreated(issue) {
-  if(issue.fields.sprint != null) {
+function checkClosedSprintAsPartOfCreation(issue, currentProcessingSprintID) {
+  if(issue.fields.closedSprints != null) {
+    for(var i = 0; i <issue.fields.closedSprints.length; i++) {
+      if(issue.fields.closedSprints[i].id == currentProcessingSprintID) {
+        checkSprintAsPartOfCreation(issue, issue.fields.closedSprints[i]);
+      }
+    }
+  }
+}
+
+function checkAddedToActiveSprintAfterCreated(issue, currentProcessingSprintID) {
+  if(issue.fields.sprint != null && issue.fields.sprint.id == currentProcessingSprintID) {
     checkAddedToSprintAfterCreated(issue, issue.fields.sprint);
   }
 }
 
 function checkAddedToSprintAfterCreated(issue, sprint) {
 var sprintName = sprint.name;
-  var sprintCreatedTimestamp = sprint.startDate;
+  var sprintStartDate = sprint.startDate;
   for(var i = 0; i < issue.changelog.histories.length; i++) {
     var currentHistory = issue.changelog.histories[i];
     for(var j = 0; j < currentHistory.items.length; j++) {
       var historyItem = currentHistory.items[j];
       var fieldChange = historyItem.field;
       if(fieldChange == "Sprint") {
-        var timestampOfChange = currentHistory.created;
+        //thanks JIRA for storing some dates in UTC and some in local time. sprint start date is stored UTC
+        var timeZone = currentHistory.author.timeZone;
+        var timestampOfChange = moment(currentHistory.created).tz(timeZone);
         var sprintString = historyItem.toString;
-        if(sprintString.includes(sprintName) && timestampOfChange > sprintCreatedTimestamp) {
-          addedIssues[sprint.id + '-' + issue.id] = issue.key;
+        if(sprintString.includes(sprintName) && timestampOfChange > moment(sprintStartDate)) {
+          if(addedIssues[sprint.id] == null) {
+            addedIssues[sprint.id] = {};
+            addedIssues[sprint.id][issue.id] = issue.key;//{id:issue.id, key:issue.key};
+          }
+          else {
+            addedIssues[sprint.id][issue.id] = issue.key;
+          }
           return;
         }
       }
@@ -139,10 +160,12 @@ var sprintName = sprint.name;
   }
 }
 
-function checkAddedToClosedSprintAfterCreated(issue) {
+function checkAddedToClosedSprintAfterCreated(issue, currentProcessingSprintID) {
   if(issue.fields.closedSprints != null) {
     for(var i = 0; i <issue.fields.closedSprints.length; i++) {
-      checkAddedToSprintAfterCreated(issue, issue.fields.closedSprints[i]);
+      if(issue.fields.closedSprints[i].id == currentProcessingSprintID) {
+        checkAddedToSprintAfterCreated(issue, issue.fields.closedSprints[i]);
+      }
     }
   }
 }
