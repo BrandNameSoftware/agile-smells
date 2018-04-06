9cd06ea03c73cf0db73d10462b1b52d835c56cf5
Matthew Brand <mbrand@gmail.com>
2018-04-06 08:59:36 -0600

    Hide/show is working

--- agile-smells/public/js/utils.js
+++ agile-smells/public/js/utils.js
@@ -1,9 +1,18 @@
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function collapseDiv() {
+  var readBlock = document.getElementById('collapse-content');
+  if (readBlock.className == "collapse in") {
+    readBlock.className = "collapse";
+  } else {
+    readBlock.className = "collapse in";
+  }
 }
